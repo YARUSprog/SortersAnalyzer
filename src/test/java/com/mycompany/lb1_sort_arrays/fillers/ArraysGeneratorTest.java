a9182fdebaeece58ddf932c3d6a4b95c20a441c3
Musienko Yaroslav <YARUSprog@mail.ru>
2017-06-24 22:56:48 +0300

    Removed unnecessary comments

--- src/test/java/com/mycompany/lb1_sort_arrays/fillers/ArraysGeneratorTest.java
+++ src/test/java/com/mycompany/lb1_sort_arrays/fillers/ArraysGeneratorTest.java
@@ -1,11 +1,6 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package com.mycompany.lb1_sort_arrays.fillers;
 
-import com.mycompany.lb1_sort_arrays.sorters.BubleSortFromFirst;
 import java.util.Arrays;
 import org.junit.After;
 import org.junit.AfterClass;
--- src/test/java/com/mycompany/lb1_sort_arrays/sorters/QuickSortTest.java
+++ src/test/java/com/mycompany/lb1_sort_arrays/sorters/QuickSortTest.java
@@ -1,8 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package com.mycompany.lb1_sort_arrays.sorters;
 
 import java.util.Arrays;
@@ -10,7 +6,6 @@ import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
 import org.junit.BeforeClass;
-import org.junit.Test;
 import static org.junit.Assert.*;
 
 /**
